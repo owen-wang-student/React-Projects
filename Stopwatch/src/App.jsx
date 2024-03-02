6855a6f061f3ab7a42f8e7e2badc0843039efdb2
owen-wang-student <owang420@gmail.com>
2024-03-01 18:53:29 -0600

    configured tailwind css

---
+++ Stopwatch/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className='text-6xl text-red-500'>Tailwind Test</h1>
+    </div>
+  ) 
+}
+
+export default App
