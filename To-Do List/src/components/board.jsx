66d8024bc0ef9b003255d73bcc79a7f0f1d03d0e
owen-wang-student <owang420@gmail.com>
2024-03-11 21:30:17 -0500

    removed project

--- To-Do List/src/components/board.jsx
+++
@@ -1,8 +0,0 @@
-export const Board = ({task}) => {
-    return(
-        <div className="max-w-6xl flex flex-col items-center justify-center border">
-            <p>{task}</p>
-        </div>
-    )
-}
- 
